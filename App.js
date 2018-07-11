6cc4ac7476cbe8cb1a53637042de1da0144fb875
Joseph <jpcaltabiano@wpi.edu>
2018-07-11 11:26:41 -0400

    final interface update

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ export default class App extends React.Component {
       uuid: null,
       numCached: 0,
       connection: "none",
+      platform: Platform.OS
       isTracking: true
     };
 
@@ -101,8 +102,7 @@ export default class App extends React.Component {
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded'
         },
-        body: 'appkey=9355552b2db866c13b123333d10603007101597ea87b75eb3ebb370c7748fb81'
-        ,
+        body: 'appkey=9355552b2db866c13b123333d10603007101597ea87b75eb3ebb370c7748fb81&os='.concat(this.state.platform)
         method: 'POST'
       })
       .then((response) => response.json())
@@ -268,34 +268,334 @@ export default class App extends React.Component {
   },
   (error) => alert(JSON.stringify(error)),
   {enableHighAccuracy: true, distanceFilter: 5})
-
+ 
   render() {
+
+    var b1Text = Platform.OS === 'ios' ? styles.iOSB1Text : styles.androidB1Text;
+    var b2 = Platform.OS === 'ios' ? styles.iOSB2 : styles.androidB2;
+    //lat and long label
+    var b2TextLeft = Platform.OS === 'ios' ? styles.iOSB2TextLeft : styles.androidB2TextLeft;
+    //lat and long values
+    var b2TextRight = Platform.OS === 'ios' ? styles.iOSB2TextRight : styles.androidB2TextRight;
+    var b3Text = Platform.OS === 'ios' ? styles.iOSB3Text : styles.androidB3Text;
+
+
     {/*Print position to the screen*/}
     return (
+
       <View style={styles.container}>
-        <Text>Latitude: {this.state.lat}</Text>
-        <Text>Longitude: {this.state.long}</Text>
-        <Text>Time: {this.state.time.toLocaleString()}</Text>
-        <Text>UUID: {this.state.uuid}</Text>
-        <Text>numCached: {this.state.numCached}</Text>
+
+        <View style={styles.statusBar}>
+          <StatusBar/>
+        </View>
+
+        <ImageBackground
+          style={styles.imageBg}
+          source={require('./Images/acadia-national-park-maine.jpg')}
+          blurRadius={Platform.OS === 'ios' ? 5 : 1}
+        >
+
+          {this.state.fontLoaded ? (
+
+          <View style={styles.headerView}>
+            <Text style = {styles.headerText}>
+              {'Acadia Traffic Data Collection'}
+            </Text>
+          </View>
+
+          ) : null}
+
+          {this.state.fontLoaded ? (
+
+          <View style={styles.b1}>
+            <Text style={b1Text}>
+              This application will help Acadia National Park officials decrease the park's heavy traffic and
+              make visiting the park with a vehicle a less stressful experience.
+              We appreciate your support in making Acadia safer and preserving its incredible beauty.
+            </Text>
+          </View>
+
+          ) : null}
+
+          {this.state.fontLoaded ? (
+
+          <View style={b2}>
+            <Text style={b2TextLeft}>
+              Latitude:{'\n'}
+              Longitude:
+            </Text>
+
+            <Text style={b2TextRight}>
+              {this.state.lat.toFixed(3)}{'\n'}
+              {this.state.long.toFixed(3)}{'\n'}
+            </Text>
+          </View>
+
+          ) : null}
+
+          {this.state.fontLoaded ? (
+
+          <View style={styles.b3}>
+            <Text style={b3Text}>Allow Tracking</Text>
+
+            <Switch
+              style={styles.switch}
+              value={this.state.isTracking}
+              onValueChange={(value) => this.setState({isTracking: value})}
+            />
+          </View>
+
+          ) : null}
+
+          {this.state.fontLoaded ? (
+
+          <TouchableOpacity
+            style={styles.optBtn}
+            onPress={() => {
+
+              Alert.alert (
+                'Warning',
+                'Are you sure you want to permenantly delete all the data your phone has collected?',
+                [
+                  {text: 'Cancel'},
+                  {text: 'OK', onPress: () =>
+
+                    fetch('https://bhiqp.ky8.io/delete', {
+                      headers: {
+                        'Conetent-Type': 'application/x-www-form-urlencode'
+                      },
+                      body: 'uuid='.concat(this.state.uuid),
+                      method:'POST',
+                    })
+                    .then(response => this.state)
+                    .catch(error => console.error(error))
+                  }
+                ]
+              );
+
+            
+            }}
+          >
+            <Text style={styles.optBtnText}>Delete Tracking Data</Text>
+          </TouchableOpacity>
+
+          ) : null}
+          
+        </ImageBackground>
       </View>
+
     );
   }
 }
 
 AppRegistry.registerComponent('App', () => App);
 
+//b1 representing blur box 1 (top)
 const styles = StyleSheet.create({
 
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  
+  statusBar: {
+    height: Platform.OS === 'ios' ? 20 : StatusBar.currentHeight,
+    backgroundColor: '#4885ed'
+  },
+
+  imageBg: {
+    flexGrow: 1,
+    height: null,
+    width: null,
     alignItems: 'center',
     justifyContent: 'center',
   },
-    
-  TextStyle:{
+
+  headerView: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: 56,
+    justifyContent: 'center',
+    backgroundColor: '#4885ed'
+  },
+
+  headerText: {
+    position: 'absolute',
+    top: 15,
+    left: 20,
+    color: '#fff',
+    alignContent: 'flex-start',
+    fontSize: Platform.OS === 'ios' ? 20 : 25,
+    justifyContent: 'center',
+  },
+
+  b1: {
+    position: 'absolute',
+    top: 90,
+    left: 10,
+    right: 10,
+    height: 230,
+    backgroundColor: '#3d3c3c9f',
+    borderWidth: 0.5,
+    borderRadius: 2,
+    borderColor: '#282828'
+  },
+
+ androidB1Text: {
+    position: 'absolute',
+    top: 20,
+    left: 20,
+    right: 20,
+    color: '#fff',
+    textAlign:'left',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 25,
+    //fontFamily: 'Roboto-Light',
+  },
+
+  iOSB1Text:{
     color:'#fff',
-    textAlign:'center',
+    textAlign:'left',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 21,
+    borderLeftWidth: 20,
+    borderRightWidth: 20,
+    borderTopWidth: 16,
+    borderBottomWidth: 20,
+    //fontFamily: 'Roboto-Light',
+  },
+
+  iOSB2: {
+    position: 'absolute',
+    top: 356,
+    left: 10,
+    right: 10,
+    height: 80,
+    backgroundColor: '#3d3c3c9f',
+    borderWidth: 0.5,
+    borderRadius: 2,
+    borderColor: '#282828',
+    justifyContent: 'center',
+    alignContent: 'center'
+  },
+
+  androidB2: {
+    position: 'absolute',
+    top: 363,
+    left: 10,
+    right: 10,
+    height: 80,
+    backgroundColor: '#3d3c3c9f',
+    borderWidth: 0.5,
+    borderRadius: 2,
+    borderColor: '#282828',
+    justifyContent: 'center',
+    alignContent: 'center'
+  },
+
+  iOSB2TextLeft: {
+    color: '#fff',
+    textAlign:'right',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 21,
+    position: 'absolute',
+    left: 70,
+    top: 15,
+    //fontFamily: 'Roboto-Light',
+  },
+
+  androidB2TextLeft: {
+    color: '#fff',
+    textAlign:'right',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 25,
+    position: 'absolute',
+    left: 85,
+    top: 15,
+    //fontFamily: 'Roboto-Light',
+  },
+
+  iOSB2TextRight: {
+    color:'#fff',
+    textAlign:'right',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 21,
+    position: 'absolute',
+    right: 105,
+    top: 15,
+    //fontFamily: 'Roboto-Light',
+  },
+
+  androidB2TextRight: {
+    color: '#fff',
+    textAlign:'right',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 25,
+    position: 'absolute',
+    right: 125,
+    top: 15,
+    //fontFamily: 'Roboto-Light',
+  },
+
+  b3: {
+    position: 'absolute',
+    right: 10,
+    left: 10,
+    height: 60,
+    bottom: 115,
+    backgroundColor: '#3d3c3c9f',
+    borderWidth: 0.5,
+    borderRadius: 2,
+    borderColor: '#282828',
+    justifyContent: 'center',
+    alignContent: 'center'
+  },
+
+  iOSB3Text: {
+    color:'#ffffff',
+    fontSize: 21,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderLeftWidth: 20,
+  },
+
+  androidB3Text: {
+    position: 'absolute',
+    left: 20,
+    color:'#ffffff',
+    fontSize: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  switch: {
+    position: 'absolute',
+    right: 20,
+  },
+
+  optBtn: {
+    position: 'absolute',
+    bottom: 20,
+    height: 60,
+    left: 10,
+    right: 10,
+    backgroundColor: '#db3236',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 45
+  },
+
+  optBtnText: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: Platform.OS === 'ios' ? 20 : 25,
+    color: '#ffffff'
   }
 });
