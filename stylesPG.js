b8ca858ee7e85243eab8d21115bd5847f1111c62
Joseph <jpcaltabiano@wpi.edu>
2018-07-16 17:04:06 -0400

    again

--- stylesPG.js
+++ stylesPG.js
@@ -1,18 +1,18 @@
 render() {
 
-	return (
+  return (
 
-		<View style={styles.container}>
+    <View style={styles.container}>
 
-			<View style={styles.statusBar}>
-      	<StatusBar/>
+      <View style={styles.statusBar}>
+        <StatusBar/>
       </View>
 
-     	<View style={styles.headerView}>
+      <View style={styles.headerView}>
 
-     		<Text style = {styles.headerText}>
-      		{'Acadia Traffic Data Collection'}
-       	</Text>
+        <Text style = {styles.headerText}>
+          {'Acadia Traffic Data Collection'}
+        </Text>
 
       </View>
 
@@ -22,46 +22,78 @@ render() {
           make visiting the park with a vehicle a less stressful experience.
           We appreciate your support in making Acadia safer and preserving its incredible beauty.
         </Text>  
-      </View>    	
+      </View>
+
+      <View style = {styles.allowTracking}>
+        <Text style={styles.text}>Allow Tracking</Text>
+
+        <Switch
+          style={styles.switch}
+          value={this.state.isTracking}
+          onValueChange={(value) => this.setState({isTracking: value})}
+        />
+      </View>  
+
+      <TouchableOpacity
+        style={styles.optBtn}
+        onPress={() => {
+
+          Alert.alert (
+            'Warning',
+            'Are you sure you want to permenantly delete all the data your phone has collected?',
+            [
+              {text: 'Cancel'},
+              {text: 'OK', onPress: () =>
+
+                fetch('https://bhiqp.ky8.io/delete', {
+                  headers: {
+                    'Conetent-Type': 'application/x-www-form-urlencode'
+                  },
+                  body: 'uuid='.concat(this.state.uuid),
+                  method:'POST',
+                })
+                .then(response => this.state)
+                .catch(error => console.error(error))
+              }
+            ]
+          );
+        }}
+        >
+        <Text style={styles.optBtnText}>Delete Tracking Data</Text>
+      </TouchableOpacity>
 
       <View style = {styles.bottom}>
-      	<Text style = {styles.bottomText}>
-      		UUID = {this.state.UUID}{'\n'}
-      		App Icon credits: 
-      	</Text>
+        <Text style = {styles.bottomText}>
+          UUID = {this.state.UUID}{'\n'}
+          App Icon credits: 
+        </Text>
       </View>
 
     </View>
 
-
-
-	)
-
-
-
-
+  )
 
 }
 
 const styles = StyleSheet.create({
 
-	container: {
-		flex: 1,
-		backgroundColor: '#ffffff'
-	},
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff'
+  },
 
-	text: {
-		color: '#000000',
-		alignContent: 'left',
-		fontSize: Platform.OS === 'ios' ? 20 : 25,
-	}
+  text: {
+    color: '#000000',
+    alignContent: 'left',
+    fontSize: 20
+  }
 
-	statusBar: {
-		height: Platform.OS === 'ios' ? 20 : StatusBar.currentHeight,
-    	backgroundColor: '#4885ed'
-	},
+  statusBar: {
+    height: Platform.OS === 'ios' ? 20 : StatusBar.currentHeight,
+      backgroundColor: '#4885ed'
+  },
 
-	headerView: {
+  headerView: {
     position: 'absolute',
     top: 0,
     left: 0,
@@ -77,26 +109,54 @@ const styles = StyleSheet.create({
     left: 20,
     color: '#fff',
     alignContent: 'flex-start',
-    fontSize: Platform.OS === 'ios' ? 20 : 25,
+    fontSize: 20,
     justifyContent: 'center',
   },
 
   infoPara: {
-  	marginTop: 30,
-  	marginLeft: 20,
-  	alignContent: 'left'
+    position: '20%',
+    marginTop: 30,
+    marginLeft: 20,
+    alignContent: 'left'
   },
 
   bottom: {
-  	marginBottom: 20,
-  	marginLeft: 20,
-  	alignContent: 'left'
-
+    position: '90%',
+    marginBottom: 20,
+    marginLeft: 20,
+    alignContent: 'left'
   },
 
   bottomText: {
-  	fontSize: Platform.OS === 'ios' ? 10 : 15,
-  	alignContent: 'left'
+    fontSize: Platform.OS === 'ios' ? 10 : 15,
+    alignContent: 'left'
+  },
+
+  allowTracking: {
+    position: '70%'
+  }
+
+  switch: {
+    right: 20,
+  },
+
+  optBtn: {
+    position: 'absolute',
+    bottom: 20,
+    height: 60,
+    left: 10,
+    right: 10,
+    backgroundColor: '#db3236',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 45
+  },
+
+  optBtnText: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 20
+    color: '#ffffff'
   }
 
 
